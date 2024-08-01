fc8f705b03b96bc432c51aa1d598551c19bdb69a
mancinibrzl <mariomancinineto2017@gmail.com>
2024-07-31 23:01:32 -0300

    create a new laser script and add new laser sprites

---
+++ Astral Paragon - Navinha/Assets/Scripts/Lasers/LaserDoJogador.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserDoJogador : MonoBehaviour
+{
+    public float velocidadeDoLaser;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MovimentarLaser();
+    }
+
+    private void MovimentarLaser()
+    {
+        transform.Translate(Vector3.up * velocidadeDoLaser * Time.deltaTime);
+    }
+}
