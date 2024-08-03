c853a05f4708e8cd9b9d4837a7f4f786d6f160ae
mancinibrzl <mariomancinineto2017@gmail.com>
2024-08-03 00:28:15 -0300

    Update Inimigos.cs

--- Astral Paragon - Navinha/Assets/Scripts/Enemies/Inimigos.cs
+++ Astral Paragon - Navinha/Assets/Scripts/Enemies/Inimigos.cs
@@ -13,11 +13,14 @@ public class Inimigos : MonoBehaviour
 
     public bool inimigoAtirador;
 
+    public int vidaMaximaDoInimigo;
+    public int vidaAtualDoInimigo;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        vidaAtualDoInimigo = vidaMaximaDoInimigo;
     }
 
     // Update is called once per frame
@@ -44,6 +47,16 @@ public class Inimigos : MonoBehaviour
         {
             Instantiate(laserDoInimigo, localDoDisparo.position, Quaternion.Euler(0f, 0f, 90f));
             tempoAtualDosLasers = tempoMaximoEntreOsLasers;
+        }
+    }
+
+    public void MachucarInimigo(int danoParaReceber)
+    {
+        vidaAtualDoInimigo -= danoParaReceber;
+
+        if (vidaAtualDoInimigo <= 0)
+        {
+            Destroy(this.gameObject);
         }
     }
 
