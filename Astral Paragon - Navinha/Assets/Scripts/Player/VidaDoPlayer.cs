0d1882ba7b6577f8addb3bf70df41970dbf9d3ca
mancinibrzl <mariomancinineto2017@gmail.com>
2024-08-05 11:41:57 -0300

    Update VidaDoPlayer.cs

--- Astral Paragon - Navinha/Assets/Scripts/Player/VidaDoPlayer.cs
+++ Astral Paragon - Navinha/Assets/Scripts/Player/VidaDoPlayer.cs
@@ -19,6 +19,8 @@ public class VidaDoPlayer : MonoBehaviour
     void Start()
     {
         vidaAtualDoJogador = vidaMaximaDoJogador;
+        vidaAtualDoEscudo = vidaMaximaDoEscudo;
+
         barraDeVidaDoJogador.maxValue = vidaMaximaDoJogador;
         barraDeVidaDoJogador.value = vidaAtualDoJogador;
 
@@ -38,6 +40,20 @@ public class VidaDoPlayer : MonoBehaviour
 
         escudoDoJogador.SetActive(true);
         temEscudo = true;
+    }
+
+    public void GanharVida(int vidaParaReceber)
+    {
+        if (vidaAtualDoJogador + vidaParaReceber <= vidaMaximaDoJogador)
+        {
+            vidaAtualDoJogador += vidaParaReceber;
+        }
+        else
+        {
+            vidaAtualDoJogador = vidaMaximaDoJogador;
+        }
+
+        barraDeVidaDoJogador.value = vidaAtualDoJogador;
     }
 
     public void MachucarJogador(int danoParaReceber)
